c985a384a0afc6650845cc411795746867b1c387
Oliver Stigley <oliverstigley@gmail.com>
2016-04-08 14:16:36 +1200

    styling for index complete

--- scripts/myScripts.js
+++ scripts/myScripts.js
@@ -1,21 +1,4 @@
-//  Change background colour
 
-var col=225;	
-var c=1;
-var changeBcol = function() {
-	col +=c; 
-	var newCol = "rgb("+String(col)+","+String(col)+","+String(col)+")"
-	if (col==255 || col== 224) {
-		c*=-1;
-	};
-	document.getElementsByTagName('body')[0].style.backgroundColor = newCol;
-};
-window.setInterval(changeBcol,600);
-
-var paras = document.getElementsByTagName('p');
-paras.onmouseover = function() {
-	this.style.backgroundColor = "white";
-};
 
 // /    Toggle header displays on front page. 
 $('.myheader').click(function(){
