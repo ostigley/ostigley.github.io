1b0c04bc4433baa405828ba684d164401b643836
Oliver Stigley <oliverstigley@gmail.com>
2016-04-15 12:02:40 +1200

    added blogs.js file and object to store blog info inside of

---
+++ scripts/blogs.js
@@ -0,0 +1,123 @@
+var blogList = {
+	"blogs":[ 
+
+	{
+		title: "Fireside Chat Reflections",
+		fileName: "c1-reflection-blog.html ",
+		blogdate: "23 February 2016",
+		category: "human",
+		tags: ["reflection", "EDA"]
+	}, 
+
+	{
+		title: "Time & Habits",
+		fileName: "c1-time-and-habits-blog.html ",
+		blogdate: "25 February 2016",
+		category: "human",
+		tags: ["habits", "time-management"]
+	}, 
+
+	{
+		title: "",
+		fileName: "c2-emotional-intelligence.html ",
+		blogdate: "",
+		category: "human",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "c3-meditation-process.html ",
+		blogdate: "",
+		category: "human",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "c4-daily-mindfulness.html ",
+		blogdate: "",
+		category: "human",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "c5-self-confidence.html ",
+		blogdate: "",
+		category: "human",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "c6-control.html ",
+		blogdate: "",
+		category: "human",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "c7-motivation.html ",
+		blogdate: "",
+		category: "human",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "t2-html-css-dom-p1.html ",
+		blogdate: "",
+		category: "dev",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "t2-html-css-dom-p2.html ",
+		blogdate: "",
+		category: "dev",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "t3-design-to-web-blog.html ",
+		blogdate: "",
+		category: "dev",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "t4-javascript-basics.html ",
+		blogdate: "",
+		category: "dev",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "t5-problem-solving.html ",
+		blogdate: "",
+		category: "dev",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "t6-js-language.html ",
+		blogdate: "",
+		category: "dev",
+		tags: []
+	}, 
+
+	{
+		title: "",
+		fileName: "t7-testing.html ",
+		blogdate: "",
+		category: "dev",
+		tags: []
+	}, 
+};
